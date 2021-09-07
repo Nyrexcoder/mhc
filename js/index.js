739b7f052f92f77bc01a62fd6ebdb8b24482df52
Nyrexcoder <88071981+Nyrexcoder@users.noreply.github.com>
2021-09-07 17:42:30 +0530

    files update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
+
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show')
+      })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Services
+
+$(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
+
 
 // read more and read less button
 const readMoreBtn = document.querySelector('.read-more-btn');
