1796417b1407199c6b526bbf10d6da002a11bdf5
Nyrexcoder <88071981+Nyrexcoder@users.noreply.github.com>
2021-09-09 19:32:31 +0530

    files update

---
+++ js/index.js
@@ -0,0 +1,102 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
+
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show')
+      })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Services
+
+$(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
+
+
+//............................................................//
+
+// Pass value click to input tag
+
+function gstR(count){
+  document.getElementById('purpose').value=count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// read more and read less button
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text');
+
+readMoreBtn.addEventListener('click',(e)=>{
+    text.classList.toggle('show-more');
+    if(readMoreBtn.innerText === 'Read More'){
+        readMoreBtn.innerText = 'Read Less';
+    }
+    else{
+        readMoreBtn.innerText = 'Read More';
+    }
+})
+
+//confirm box
+const send = document.querySelector('.send');
+
+send.addEventListener('click', (e)=>{
+  if (confirm("Are you sure to submit your form ?") == true) {
+    userPreference = "form succesfully submited";
+} else {
+  alert("form not submit !");
+  e.preventDefault(); 
+}
+});
+
+
