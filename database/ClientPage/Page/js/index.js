94c68baea7f7d38e7e5a94773ab92336e0a6f4f7
Nyrexcoder <88071981+Nyrexcoder@users.noreply.github.com>
2021-09-30 10:56:23 +0530

    update website

--- database/ClientPage/Page/js/index.js
+++ database/ClientPage/Page/js/index.js
@@ -48,22 +48,18 @@ function gstR(count){
 }
 
 // read more and read less button
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text');
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
-
+readMoreBtn.addEventListener('click',(e)=>{
+    text.classList.toggle('show-more');
+    if(readMoreBtn.innerText === 'Read More'){
+        readMoreBtn.innerText = 'Read Less';
+    }
+    else{
+        readMoreBtn.innerText = 'Read More';
+    }
+})
 
 // confirm box
 const send = document.querySelector('.send');
