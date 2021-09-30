856675a359ae27c07a295891171cc66495a25c26
Nyrexcoder <88071981+Nyrexcoder@users.noreply.github.com>
2021-09-30 12:07:10 +0530

    update pages

--- database/ClientPage/Page/js/index.js
+++ database/ClientPage/Page/js/index.js
@@ -48,18 +48,21 @@ function gstR(count){
 }
 
 // read more and read less button
-const readMoreBtn = document.querySelector('.read-more-btn');
-const text = document.querySelector('.text');
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
-readMoreBtn.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
-    if(readMoreBtn.innerText === 'Read More'){
-        readMoreBtn.innerText = 'Read Less';
-    }
-    else{
-        readMoreBtn.innerText = 'Read More';
-    }
-})
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
 // confirm box
 const send = document.querySelector('.send');
