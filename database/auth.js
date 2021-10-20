3605d5c91545c51e9fc83e96ec01db67b8f0bc04
Nyrexcoder <88071981+Nyrexcoder@users.noreply.github.com>
2021-10-20 12:19:29 +0530

    update Facebook login method

--- database/auth.js
+++ database/auth.js
@@ -106,15 +106,46 @@ function goolelog(){
   });
 }
 
+// Login wigh facebook function
+function facbooklog(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    window.location.href="/logout.html";
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
 
-// Logout form
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-    alert("user Logout successfully")
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    alert(error);
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    // ...
   });
-});
+}
+// Logout form
+// const logout = document.querySelector('#logout');
+// logout.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   auth.signOut().then(() => {
+//     alert("user Logout successfully")
+//   });
+// });
 
 // Forget password
 function myFunction(){
